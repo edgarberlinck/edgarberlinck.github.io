610dfea0b9fb4b186d958c85fdcef37f89d219fd
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2015-04-15 21:10:04 -0300

    Trabalho no layout

--- notifly/notifly.js
+++ notifly/notifly.js
@@ -1,9 +1,204 @@
 Parse.initialize("VAlZMToAqr3mIJymUds3F2oCNWRyPoHFB6Vc7AoW", "ELd5pPxzQQy2t5M7tl63AgZowLQwkgJ3JKr3DZ0x");
 
 var content = document.getElementById("content");
+var messages = document.getElementById("messages");
 
-function home() {
+function home(message) {
+	if(message) {
+		messages.classList.remove("alert-danger");
+		messages.classList.add("alert-success");
+		messages.children[0].innerHTML = message;
+		messages.classList.remove("hide");
+		messages.classList.add("show");
+	}
 	goTo("notifly/addquery.html", configureQuery);	
+}
+
+
+function search() {
+	messages.classList.add("hide");
+	messages.classList.remove("show");
+
+	$(".has-error").removeClass("has-error");
+
+	var origem = document.getElementById("origem");
+	var destino = document.getElementById("destino");
+	var ida = document.getElementById("ida");
+	var volta = document.getElementById("volta");
+	var adultos = document.getElementById("adultos");
+	var criancas = document.getElementById("criancas");
+	var bebes = document.getElementById("bebes");
+	var valor = document.getElementById("valor");
+	var hasErrors = false;
+	
+	if (origem.value === "") {
+		hasErrors = true;
+		origem.parentNode.parentNode.classList.add("has-error");
+	}
+
+	if (destino.value === "") {
+		hasErrors = true;
+		destino.parentNode.parentNode.classList.add("has-error");
+	}
+
+	if (ida.value === "") {
+		hasErrors = true;
+		ida.parentNode.classList.add("has-error");	
+	}
+
+	if (adultos.value === "" && criancas.value === "" && bebes.value === "") {
+		hasErrors = true;
+		adultos.parentNode.classList.add("has-error");	
+	}
+
+	if (valor.value === "") {
+		hasErrors = true;
+		valor.parentNode.classList.add("has-error");	
+	}
+
+	if (hasErrors) {
+		messages.classList.remove("hide");
+		messages.classList.add("show");
+	} else {
+
+		var search = new Object();
+		search.origem = origem.value;
+		search.destino = destino.value;
+		search.ida = ida.value;
+		search.volta = volta.value;
+		search.adultos = adultos.value;
+		search.criancas = criancas.value;
+		search.bebes = bebes.value;
+		search.valor = valor.value;
+
+		sessionStorage.setItem("search", JSON.stringify(search));
+
+		var currentUser = Parse.User.current();
+
+		if (!currentUser) {
+			goTo("notifly/register.html");
+		} else {
+			salvar(search);
+		}
+	}
+
+}
+
+function registrar() {
+	messages.classList.add("hide");
+	messages.classList.remove("show");
+
+	var nome = document.getElementById("nomeCompleto");
+	var email = document.getElementById("email");
+	var senha = document.getElementById("senha");
+	var senha2 = document.getElementById("senha2");
+	var hasErrors = false;
+
+	if (nome.value === "") {
+		hasErrors = true;
+		nome.parentNode.classList.add("has-error");
+	}
+
+	if (email.value === "") {
+		hasErrors = true;
+		email.parentNode.classList.add("has-error");
+	}
+
+	if (senha.value === "") {
+		hasErrors = true;
+		senha.parentNode.classList.add("has-error");
+	}
+
+	if (senha2.value === "") {
+		hasErrors = true;
+		senha2.parentNode.classList.add("has-error");
+	}
+
+	if (!hasErrors) {
+		if (senha.value !== senha2.value) {
+			messages.children[0].innerHTML = "As senhas devem ser iguais :("
+			messages.classList.remove("hide");
+			messages.classList.add("show");
+		} else {
+			var newuser = new Parse.User();
+			newuser.set("username", email.value);
+			newuser.set("password", senha.value);
+			newuser.set("email", email.value);
+			newuser.set("nome", nome.value);
+			
+			newuser.signUp().then(function(user){
+				salvar(user);
+			});
+		}
+	} else {
+		messages.classList.remove("hide");
+		messages.classList.add("show");
+	}
+}
+
+function logar(user) {
+	messages.classList.add("alert-danger");
+	messages.classList.remove("alert-success");
+	messages.classList.add("hide");
+	messages.classList.remove("show");
+
+	var username;
+	var password;
+
+	if (!user){
+		username = document.getElementById("username").value;
+		password = document.getElementById("password").value;
+	} else {
+		username = user.get("username");
+		password = user.get("password");
+	}
+
+	Parse.User.logIn(username, password).then(
+	function(){
+		var search = sessionStorage.getItem("search");
+		if (search) {
+			search = JSON.parse(search);
+			salvar(search);
+		} else {
+			home();
+		}
+	}, 
+	function() {
+		messages.classList.add("alert-danger");
+		messages.classList.remove("alert-success");
+		messages.children[0].innerHTML = "Usuário / Senha incorretos.";
+		messages.classList.remove("hide");
+		messages.classList.add("show");
+	});
+}
+
+function salvar(search) {
+	var currentUser = Parse.User.current();
+	if (!currentUser) {
+		goTo("notifly/register.html");
+	} else {
+		var Busca = Parse.Object.extend("busca");
+		var busca = new Busca();
+		
+		if (!search) {
+			search = JSON.parse(sessionStorage.getItem("search"));
+		}
+
+		busca.set("origem", search.origem);
+		busca.set("destino", search.destino);
+		busca.set("ida", search.ida);
+		busca.set("volta", search.volta);
+		busca.set("adultos", search.adultos);
+		busca.set("criancas", search.criancas);
+		busca.set("bebes", search.bebes);
+		busca.set("valor", search.valor);
+		busca.relation("user").add(currentUser);
+		
+		busca.save().then(function(busca){
+			home("Ok. Avisaremos quando encontramos!");
+			sessionStorage.removeItem("search");
+		});
+	}
 }
 
 function goTo(page, complete) {
@@ -23,28 +218,16 @@ function goTo(page, complete) {
 
 function configureQuery() {
     var availableAirports = [
-      "ActionScript",
-      "AppleScript",
-      "Asp",
-      "BASIC",
-      "C",
-      "C++",
-      "Clojure",
-      "COBOL",
-      "ColdFusion",
-      "Erlang",
-      "Fortran",
-      "Groovy",
-      "Haskell",
-      "Java",
-      "JavaScript",
-      "Lisp",
-      "Perl",
-      "PHP",
-      "Python",
-      "Ruby",
-      "Scala",
-      "Scheme"
+      "VIX - Aeroporto Eurico de Aguiar Salles",
+      "CNF - Aeroporto Internacional Tancredo Neves",
+      "REC - Aeroporto Internacional dos Guararapes Gilberto Freyre",
+      "PNZ - Aeroporto de Petrolin",
+      "FEN - Aeroporto de Fernando de Noronha",
+      "GRU - Aeroporto Internacional de São Paulo-Guarulhos",
+      "VCP - Aeroporto Internacional de Viracopos-Campinas",
+      "CGH - Aeroporto de Congonhas/São Paulo",
+      "GIG - Aeroporto Internacional do Rio de Janeiro",
+      "SDU - Aeroporto Santos Dumont"
     ];
 
 	var substringMatcher = function(strs) {
